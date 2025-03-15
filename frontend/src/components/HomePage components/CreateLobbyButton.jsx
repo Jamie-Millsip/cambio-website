f73ce7a3b89bde7e785de50b94d4bc467b401adb
psyjm30 <psyjm30@nottingham.ac.uk>
2025-03-15 14:25:48 +0000

    updated .gitignore

--- frontend/src/components/HomePage components/CreateLobbyButton.jsx
+++ frontend/src/components/HomePage components/CreateLobbyButton.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import "../pages/Body.css"
+import "../../pages/Body.css"
 
 
 function CreateLobbyButton({}){
