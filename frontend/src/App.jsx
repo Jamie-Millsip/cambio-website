83cc567d58c9a609034c93e8aeb18f8ac87dbbc8
psyjm30 <psyjm30@nottingham.ac.uk>
2025-03-18 16:59:53 +0000

    added basename to routing to

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App(){
   return(
     <>
       <Header/>
-        <Router>
+        <Router basename='Cambio-Website'>
           <Routes>
             <Route path = "/" element = {<HomePage/>}/>
             <Route path = "/lobby/:lobbyID" element = {<LobbyPage/>}/>
