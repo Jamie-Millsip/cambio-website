f53b135b26c6630c5e78df4426c96ea7930353d6
psyjm30 <psyjm30@nottingham.ac.uk>
2025-03-19 14:22:50 +0000

    test hash routing --- COMMIT BEFORE THIS FOR GOOD VERSION

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import HomePage from './pages/HomePage';
 import LobbyPage from './pages/Lobbypage';
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App(){
 
@@ -12,7 +12,7 @@ function App(){
   return(
     <>
       <Header/>
-        <Router basename='/Cambio-Website'>
+        <Router>
           <Routes>
             <Route path = "/" element = {<HomePage/>}/>
             <Route path = "/lobby/:lobbyID" element = {<LobbyPage/>}/>
