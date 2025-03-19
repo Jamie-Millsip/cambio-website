878f3f032463371fbb38b76496dc89451fe58c1c
psyjm30 <psyjm30@nottingham.ac.uk>
2025-03-19 02:40:55 +0000

    fix path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/Cambio-Website/frontend/`,
+  base: `/Cambio-Website/frontend/dist`,
   plugins: [react()],
   define: {
     global: 'window',
