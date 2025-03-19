77b6296f9e2ff8db76dd607b48c7f80a1885dcdd
psyjm30 <psyjm30@nottingham.ac.uk>
2025-03-19 02:46:25 +0000

    change directory to root 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/Cambio-Website/dist`,
+  base: `/Cambio-Website/`,
   plugins: [react()],
   define: {
     global: 'window',
   },
   build: {
-    outDir: `../dist`
+    outDir: `../`
   }
 })
