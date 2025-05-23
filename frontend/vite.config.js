b405a07317ae5c9a50eb7ef6d2819d553cbafbfb
psyjm30 <psyjm30@nottingham.ac.uk>
2025-05-23 23:41:01 +0100

    clean vite code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/Cambio-Website/`,
+  base: `/`,
   plugins: [react()],
   define: {
     global: 'window',
   },
   build: {
-    outDir: `../`
+    outDir: `./`
   }
 })
 
