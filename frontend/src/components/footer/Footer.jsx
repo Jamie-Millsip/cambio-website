7150d70ea3e664879707f8150f4a95a77d74574f
psyjm30 <psyjm30@nottingham.ac.uk>
2025-06-26 00:26:31 +0100

    start making css dynamic (added menu resizing to accomodate 4k screen size)

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer () {
 
     return(
         <div className="footer-container">
-            <p className="footer-text">Please disable ad-blockers for full functionality</p>
+            <p className="footer-text">Please disable ad-blockers and VPNs for full functionality</p>
         </div>
     )
 }
