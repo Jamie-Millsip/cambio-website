e74eb0bb8f74fd2f7a9d8d3ddabd63aaadeebc8c
psyjm30 <psyjm30@nottingham.ac.uk>
2025-06-25 21:46:53 +0100

    update css to scale better for mobile devices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LobbyPage from './pages/Lobbypage';
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import "./App.css"
 
 function App(){
 
@@ -10,18 +11,19 @@ function App(){
   // each lobby has unique lobbyID at end of URL
 
   return(
-    <>
+    <div className='app-container'>
       <Header/>
+      <div className='main-content'>
         <Router>
           <Routes>
             <Route path = "/" element = {<HomePage/>}/>
             <Route path = "/lobby/:lobbyID" element = {<LobbyPage/>}/>
           </Routes>
         </Router>
+        </div>
     <Footer/>
-    </>
+    </div>
   );
-    
 }
 
 export default App;
